d1ebd543317efeb0f639afd53f9d801ca1ed9f4f
tropicalmentat <gabtorres1011@gmail.com>
2021-03-14 17:39:31 +0800

    Added test LMarker

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <l-map style="height: 100%" :zoom="zoom" :center="center">
     <l-tile-layer :url="url"></l-tile-layer>
+    <l-marker :lat-lng="markerLatLng" ></l-marker>
   </l-map>
 </template>
 
 <script>
-import { LMap, LTileLayer } from "vue2-leaflet";
+import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+// import { OriginInputBox } from "./components/OriginInputBox.vue"
 
 export default {
-  components: { LMap, LTileLayer },
+  components: { LMap, LTileLayer, LMarker },
   data() {
     return {
       url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
       zoom: 13,
       center: [14.599512, 120.984222],
+      markerLatLng: [14.599512, 120.984222]
     };
   },
 };
