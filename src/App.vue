d31e1ec0cf487d3fa4632eaad44ba359c27f26e1
tropicalmentat <gabtorres1011@gmail.com>
2021-03-29 07:18:51 +0800

    Fixed fetching of route from graphhopper.

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
           </v-text-field>
           <v-text-field label="Destination" v-model="destination">
           </v-text-field>
-          <v-btn color="grey" block @click="reverseLatLng">
+          <v-btn color="grey" block @click="getRoute">
             Plan Route
           </v-btn>
           <v-btn color="pink" block>
@@ -45,7 +45,7 @@
 
 <script>
 import { LMap, LTileLayer, LMarker, LPopup, LPolyline} from "vue2-leaflet";
-import testData from "./data/test_polyline.json"
+// import testData from "./data/test_polyline.json"
 
 export default {
   components: { LMap, LTileLayer, LMarker, LPopup, LPolyline},
@@ -58,7 +58,7 @@ export default {
       position: {},
       origin:{},
       destination:{},
-      test_data: testData,
+      // test_data: testData,
       cleaned_latlng: [],
       route: []
 
@@ -75,30 +75,31 @@ export default {
     setDestination() {
       this.destination = this.position;
     },
-    reverseLatLng() {
+    reverseLatLng(coordinates) {
       var coordinate = []
 
-      for (coordinate of this.test_data["paths"][0]["points"]["coordinates"]) {
+      for (coordinate of coordinates) {
         this.cleaned_latlng.push([coordinate[1],coordinate[0]])
       }
     },
     async getRoute() {
       let coordinates = "Unresolved";
       try {
-      const { olat, olng } = this.origin;
-      const { dlat, dlng } = this.destination;
+      // const { olat, olng } = this.origin;
+      // const { dlat, dlng } = this.destination;
       const result = await fetch (
-        `https://graphhopper.com/api/1/route?point=${olat},${olng},&point=${dlat},${dlng}&vehicle=bike&locale=en&calc_points=true&points_encoed=false&key=23959cc2-2380-4962-bb26-3746b8d7ff6b`
+        `https://graphhopper.com/api/1/route?point=${this.origin.lat},${this.origin.lng}&point=${this.destination.lat},${this.destination.lng}&vehicle=bike&locale=en&calc_points=true&points_encoded=false&key=23959cc2-2380-4962-bb26-3746b8d7ff6b`
         );
+      // alert( `https://graphhopper.com/api/1/route?point=${this.origin.lat},${this.origin.lng}&point=${this.destination.lat},${this.destination.lng}&vehicle=bike&locale=en&calc_points=true&points_encoded=false&key=23959cc2-2380-4962-bb26-3746b8d7ff6b`)
       if (result.status === 200) {
         const body = await result.json();
-        coordinates = body["paths"][0]["points"]["coordinates"][0];
+        coordinates = body["paths"][0]["points"]["coordinates"];
       }
     } catch(e) {
-      console.error("Unable to generate route->",e);
+      alert(e);
     }
     this.route = coordinates;
-    alert(route[0]);
+    alert(this.route);
     }
   },
 };
