9044d65d39274e8d3664759370dd7a6b5afeae24
tropicalmentat <gabtorres1011@gmail.com>
2021-03-29 06:42:05 +0800

    Implemented function to generate route

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,21 @@
           <v-label>
             rowt.ph
           </v-label>
-          <v-text-field label="Origin" v-model="origin">
+          <v-text-field label="Origin" placeholder="origin" v-model="origin">
           </v-text-field>
           <v-text-field label="Destination" v-model="destination">
           </v-text-field>
           <v-btn color="grey" block @click="reverseLatLng">
             Plan Route
           </v-btn>
+          <v-btn color="pink" block>
+            Reset
+          </v-btn>
         </v-col>
         <v-col>
           <LMap :zoom="zoom" :center="center" @click="updateLatLng"> 
             <l-tile-layer :url="url"></l-tile-layer>
-            <l-polyline v-if="cleaned_latlng" :lat-lngs="cleaned_latlng" :color="route.color"></l-polyline>
+            <l-polyline v-if="cleaned_latlng" :lat-lngs="cleaned_latlng" :color="green"></l-polyline>
             <l-marker  v-if="position.lat && position.lng" :lat-lng.sync="position" visible draggable>
               <l-popup>
                 <v-container>
@@ -55,12 +58,9 @@ export default {
       position: {},
       origin:{},
       destination:{},
-      route:{
-        latlngs:[[14.602685,121.033665],[14.552686,121.051559]],
-        color: 'green'
-      },
       test_data: testData,
-      cleaned_latlng: []
+      cleaned_latlng: [],
+      route: []
 
     };
   },
@@ -81,8 +81,24 @@ export default {
       for (coordinate of this.test_data["paths"][0]["points"]["coordinates"]) {
         this.cleaned_latlng.push([coordinate[1],coordinate[0]])
       }
-
-      alert(this.cleaned_latlng[0]);
+    },
+    async getRoute() {
+      let coordinates = "Unresolved";
+      try {
+      const { olat, olng } = this.origin;
+      const { dlat, dlng } = this.destination;
+      const result = await fetch (
+        `https://graphhopper.com/api/1/route?point=${olat},${olng},&point=${dlat},${dlng}&vehicle=bike&locale=en&calc_points=true&points_encoed=false&key=23959cc2-2380-4962-bb26-3746b8d7ff6b`
+        );
+      if (result.status === 200) {
+        const body = await result.json();
+        coordinates = body["paths"][0]["points"]["coordinates"][0];
+      }
+    } catch(e) {
+      console.error("Unable to generate route->",e);
+    }
+    this.route = coordinates;
+    alert(route[0]);
     }
   },
 };
