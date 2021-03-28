812a9eb064972e13634a66acc3ca7fc91833e9c7
tropicalmentat <gabtorres1011@gmail.com>
2021-03-28 15:51:33 +0800

    Implemented function to reverse longlat result from Graphhopper to latlong

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,24 @@
   <v-app>
       <v-row>
         <v-col lg="3">
-            <v-label>
-              rowt.ph
-            </v-label>
-            <v-text-field label="Origin" v-model="origin">
-            </v-text-field>
-            <v-text-field label="Destination" v-model="destination">
-            </v-text-field>
+          <v-label>
+            rowt.ph
+          </v-label>
+          <v-text-field label="Origin" v-model="origin">
+          </v-text-field>
+          <v-text-field label="Destination" v-model="destination">
+          </v-text-field>
+          <v-btn color="grey" block @click="reverseLatLng">
+            Plan Route
+          </v-btn>
         </v-col>
         <v-col>
           <LMap :zoom="zoom" :center="center" @click="updateLatLng"> 
             <l-tile-layer :url="url"></l-tile-layer>
+            <l-polyline v-if="cleaned_latlng" :lat-lngs="cleaned_latlng" :color="route.color"></l-polyline>
             <l-marker  v-if="position.lat && position.lng" :lat-lng.sync="position" visible draggable>
               <l-popup>
-                <V-container>
+                <v-container>
                   <v-btn color="green" block @click="setOrigin">
                     Set Origin
                   </v-btn>
@@ -27,6 +31,7 @@
             </l-marker>
             <l-marker id="origin" v-if="origin.lat && origin.lng" :lat-lng.sync="origin"></l-marker>
             <l-marker id="destination" v-if="destination.lat && destination.lng" :lat-lng.sync="destination"></l-marker>
+            
           </LMap>
         </v-col>
       </v-row>
@@ -36,12 +41,11 @@
 
 
 <script>
-import { LMap, LTileLayer, LMarker, LPopup } from "vue2-leaflet";
-// import { LatLng } from "leaflet"
-
+import { LMap, LTileLayer, LMarker, LPopup, LPolyline} from "vue2-leaflet";
+import testData from "./data/test_polyline.json"
 
 export default {
-  components: { LMap, LTileLayer, LMarker, LPopup },
+  components: { LMap, LTileLayer, LMarker, LPopup, LPolyline},
   data() {
     return {
       url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
@@ -51,17 +55,17 @@ export default {
       position: {},
       origin:{},
       destination:{},
+      route:{
+        latlngs:[[14.602685,121.033665],[14.552686,121.051559]],
+        color: 'green'
+      },
+      test_data: testData,
+      cleaned_latlng: []
 
     };
   },
   methods: {
-    // getLatLng(e) {
-    //   alert(e.latlng.lat + "," + e.latlng.lng)
-    //   // _latlng = LMap.latlng(e.latlng.lat,e.latlng.lng)
-    // },
-
     updateLatLng(e) {
-      // alert(e.latlng.lat + "," + e.latlng.lng)
       this.position = e.latlng;
     },
     setOrigin() {
@@ -70,11 +74,19 @@ export default {
     },
     setDestination() {
       this.destination = this.position;
+    },
+    reverseLatLng() {
+      var coordinate = []
+
+      for (coordinate of this.test_data["paths"][0]["points"]["coordinates"]) {
+        this.cleaned_latlng.push([coordinate[1],coordinate[0]])
+      }
+
+      alert(this.cleaned_latlng[0]);
     }
   },
 };
 </script>
 
 <style>
-
 </style>
