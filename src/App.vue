fa50a34d998e790181f8d4910c141a9ad1ee2186
tropicalmentat <gabtorres1011@gmail.com>
2021-04-01 06:51:30 +0800

    Added v-card for distance of planned route

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,48 @@
 <template>
-  <v-app>
-      <v-row>
-        <v-col lg="3">
-          <v-label>
-            rowt.ph
-          </v-label>
-          <v-text-field label="Origin" placeholder="origin" v-model="origin">
-          </v-text-field>
-          <v-text-field label="Destination" v-model="destination">
-          </v-text-field>
-          <v-btn color="grey" block @click="getRoute">
-            Plan Route
-          </v-btn>
-          <v-btn color="pink" @click="resetData" block>
-            Reset
-          </v-btn>
-        </v-col>
-        <v-col>
-          <LMap :zoom="zoom" :center="center" @click="updateLatLng"> 
-            <l-tile-layer :url="url"></l-tile-layer>
-            <l-polyline v-if="cleaned_latlng" :lat-lngs="cleaned_latlng" :color="green"></l-polyline>
-            <l-marker  v-if="position.lat && position.lng" :lat-lng.sync="position" visible draggable>
-              <l-popup>
-                <v-container>
-                  <v-btn color="green" block @click="setOrigin">
-                    Set Origin
-                  </v-btn>
-                  <v-btn color="red" block @click="setDestination">
-                    Set Destination
-                  </v-btn>
-                </v-container>
-              </l-popup>
-            </l-marker>
-            <l-marker id="origin" v-if="origin.lat && origin.lng" :lat-lng.sync="origin"></l-marker>
-            <l-marker id="destination" v-if="destination.lat && destination.lng" :lat-lng.sync="destination"></l-marker>
-            
-          </LMap>
-        </v-col>
-      </v-row>
-  </v-app>
+	<v-app>
+		<v-row>
+			<v-col lg="3">
+				<v-container fluid>
+				<v-label>
+					rowt.ph
+				</v-label>
+				<v-text-field label="Origin" placeholder="origin" v-model="origin">
+				</v-text-field>
+				<v-text-field label="Destination" v-model="destination">
+				</v-text-field>
+				<v-btn color="grey" block @click="getRoute">
+					Plan Route
+				</v-btn>
+				<v-btn color="pink" @click="resetData" block>
+					Reset
+				</v-btn>
+				<v-card>
+					Distance:  {{ distance/1000 }} km
+				</v-card>
+			</v-container>
+			</v-col>
+			<v-col>
+				<LMap :zoom="zoom" :center="center" @click="updateLatLng"> 
+					<l-tile-layer :url="url"></l-tile-layer>
+					<l-polyline v-if="cleaned_latlng" :lat-lngs="cleaned_latlng" :color="green"></l-polyline>
+					<l-marker  v-if="position.lat && position.lng" :lat-lng.sync="position" visible draggable>
+					<l-popup>
+					<v-container>
+					<v-btn color="green" block @click="setOrigin">
+					Set Origin
+					</v-btn>
+					<v-btn color="red" block @click="setDestination">
+					Set Destination
+					</v-btn>
+					</v-container>
+					</l-popup>
+					</l-marker>
+					<l-marker id="origin" v-if="origin.lat && origin.lng" :lat-lng.sync="origin"></l-marker>
+					<l-marker id="destination" v-if="destination.lat && destination.lng" :lat-lng.sync="destination"></l-marker>
+				</LMap>
+			</v-col>
+		</v-row>
+	</v-app>
 </template>
 
 
@@ -58,8 +62,8 @@ export default {
       origin:{},
       destination:{},
       cleaned_latlng: [],
-      route: []
-
+      route: [],
+      distance: ""
     };
   },
   methods: {
@@ -86,9 +90,11 @@ export default {
       this.route = []
       this.cleaned_latlng = []
       this.position = {}
+      this.distance = ""
     },
     async getRoute() {
       let coordinates = "Unresolved";
+      let distance = "Unresolved";
       try {
       const result = await fetch (
         `https://graphhopper.com/api/1/route?point=${this.origin.lat},${this.origin.lng}&point=${this.destination.lat},${this.destination.lng}&vehicle=bike&locale=en&calc_points=true&points_encoded=false&key=23959cc2-2380-4962-bb26-3746b8d7ff6b`
@@ -96,11 +102,13 @@ export default {
       if (result.status === 200) {
         const body = await result.json();
         coordinates = body["paths"][0]["points"]["coordinates"];
+        distance = body["paths"][0]["distance"]
       }
     } catch(e) {
       alert(e);
     }
     this.route = coordinates;
+    this.distance = distance;
 
     // This is done to swap lng,lat in the result to lat,lng for the polyline to render
     var coordinate = []
